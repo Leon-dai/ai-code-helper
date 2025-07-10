e271b612621d3495e8f50bbe6d7ff15d98457737
leon <212649354@qq.com>
2025-07-10 20:09:05 +0800

    1.AI编程小助手项目初始化+AI对话ChatModel

---
+++ src/test/java/com/leon/aicodehelper/AiCodeHelperApplicationTests.java
@@ -0,0 +1,17 @@
+package com.leon.aicodehelper;
+
+import com.leon.aicodehelper.ai.AiCodeHelper;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeHelperApplicationTests {
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+
+    @Test
+    void chat() {
+        aiCodeHelper.chat("你好，我是程序员leon");
+    }
+}
