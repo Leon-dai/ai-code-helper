e4c6c17f5aea1681896c4e4e45e3a4ee7e480086
leon <212649354@qq.com>
2025-07-10 20:19:09 +0800

    2.多模态

--- src/test/java/com/leon/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/com/leon/aicodehelper/AiCodeHelperApplicationTests.java
@@ -1,6 +1,9 @@
 package com.leon.aicodehelper;
 
 import com.leon.aicodehelper.ai.AiCodeHelper;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,5 +16,14 @@ class AiCodeHelperApplicationTests {
     @Test
     void chat() {
         aiCodeHelper.chat("你好，我是程序员leon");
+    }
+
+    @Test
+    void chatwithMessage() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://www.codefather.cn/logo.png")
+        );
+        aiCodeHelper.chatwithMessage(userMessage);
     }
 }
