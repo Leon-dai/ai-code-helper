6fd4338dbb625d44b73238d30b9b5aadfe74c0a7
leon <212649354@qq.com>
2025-07-13 12:19:59 +0800

    6. 结构化输出

--- src/main/java/com/leon/aicodehelper/ai/AiCodeHelperService.java
+++ src/main/java/com/leon/aicodehelper/ai/AiCodeHelperService.java
@@ -3,8 +3,15 @@ package com.leon.aicodehelper.ai;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.spring.AiService;
 
+import java.util.List;
+
 //@AiService
 public interface AiCodeHelperService {
     @SystemMessage(fromResource = "system-prompt.txt")
     String chat(String userMessage);
+
+    @SystemMessage(fromResource ="system-prompt.txt")
+    Report chatForReport(String userMessage);
+    // 学习报告
+    record Report(String name, List<String> suggestionList){}
 }
--- src/test/java/com/leon/aicodehelper/ai/AiCodeHelperServiceTest.java
+++ src/test/java/com/leon/aicodehelper/ai/AiCodeHelperServiceTest.java
@@ -17,4 +17,11 @@ class AiCodeHelperServiceTest {
         String result = aiCodeHelperService.chat("你好，我是程序员leon");
         System.out.println("result = " + result);
     }
+
+    @Test
+    void chatForReport() {
+        String userMessage = "你好，我是程序员leon，学编程两年半，请帮我制定学习报告";
+        AiCodeHelperService.Report report = aiCodeHelperService.chatForReport(userMessage);
+        System.out.println(report);
+    }
 }
